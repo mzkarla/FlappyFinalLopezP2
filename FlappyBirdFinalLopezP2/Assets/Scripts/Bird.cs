61ad554956756d10c54dbc56941788cbde2fc3fc
mzkarla <714875@student.dusd.net>
2022-12-19 16:50:49 -0800

    Animated
    Animated the bird

--- FlappyBirdFinalLopezP2/Assets/Scripts/Bird.cs
+++ FlappyBirdFinalLopezP2/Assets/Scripts/Bird.cs
@@ -47,5 +47,6 @@ public class Bird : MonoBehaviour
         //...tell the Animator about it...
         anim.SetTrigger("Die");
         //...and tell the game control about it.
+        GameControl.instance.BirdDied();
     }
 }
